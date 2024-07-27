22d86ddf54b850862564d37642ca7ecafdc773ee
akmtasdikulislam <akmtasdikulislam@gmail.com>
2024-07-27 19:52:35 +0600

    created HOME Component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main id="home">
+      <div className="container">
+        <h1>Discover Our Best-Selling Products</h1>
+        <div className="row">
+          <div className="col-9"></div>
+          <div className="col-3"></div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
